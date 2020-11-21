fc8ab70f62ec4b21afa28ad357187c9457315fa4
Bobik Adrián Márk <asdbobik0@gmail.com>
2020-11-21 17:20:30 +0100

    Hello world!

--- HelloWorld/src/gites/Gites.java
+++ HelloWorld/src/gites/Gites.java
@@ -16,7 +16,7 @@ public class Gites {
      */
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Márk vagyok!");
+        System.out.println("Mark vagyok!");
     }
     
 }
