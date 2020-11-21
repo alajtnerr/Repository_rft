dd54e26f0140f16735b942d85f3fb0a1e4ab028a
Bobik Adrián Márk <asdbobik0@gmail.com>
2020-11-21 17:26:54 +0100

    Hello world!

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -16,7 +16,7 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Valami");
+        System.out.println("Valami_valami");
     }
     
 }
